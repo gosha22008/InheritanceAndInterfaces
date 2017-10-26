ec64eb5dffdf516f38ce3b79e681c325d4b09034
gosha22008 <gosha22008@yandex.ru>
2017-10-26 19:37:22 +0300

    asdf

--- 123.php
+++ 123.php
@@ -1,52 +1,109 @@
 <?php
 
+interface PriceWithDiscount
+{
+    public function getPriceWithDiscount();
+
+}
+
+interface DiscountWeight extends PriceWithDiscount
+{
+    public function getDiscountWeight();
+}
+
 class Product
 {
     private $price;
     private $title;
+    private $discount = 10;
+    private $delivery;
 
     public function __construct($price, $title)
     {
-        return $this->price = $price;
-        return $this->title = $title;
-
+        $this->price = $price;
+        $this->title = $title;
     }
 
     public function getPrice()
     {
         return $this->price;
-        return $this;
     }
 
     public function getTitle()
     {
         return $this->title;
-        return $this;
     }
 
+    public function getDiscount()
+    {
+        return $this->discount;
+    }
 
+    public function setDelivery($delivery)
+    {
+        $this->delivery = $delivery;
+    }
+    public function getDelivery()
+    {
+        return $this->delivery;
+    }
 }
 
-class Bananas extends Product
+class Bananas extends Product implements DiscountWeight
 {
     private $weight;
+
+    public function __construct($price, $title, $weight)
+    {
+        parent::__construct($price, $title);
+        $this->weight = $weight;
+    }
+
+    public function getPriceWithDiscount()
+    {
+        // TODO: Implement getPriceWithDiscount() method.
+        return $this->getPrice() - ($this->getPrice() / 100) * $this->getDiscount();
+    }
+
+    public function getDiscountWeight()
+    {
+        // TODO: Implement getDiscountWeight() method.
+        if ($this->getWeight() > 10) {
+            $this->setDelivery(300);
+            return 'Цена со скидкой : ' . $this->getPriceWithDiscount();
+
+        } else {
+            $this->setDelivery(250);
+            return 'Цена : ' . $this->getPrice() . '.' . 'Скидки нет , так как вес меньше 10 кг.';
+
+        }
+    }
 
     public function getWeight()
     {
         return $this->weight;
-        return $this;
     }
 
 }
 
-class Wine extends Product
+class Wine extends Product implements PriceWithDiscount
 {
-    private $sort;
+    private $sort = 'Красное';
+
+    public function __construct($price, $title)
+    {
+        parent::__construct($price, $title);
+    }
 
     public function getSort()
     {
         return $this->sort;
-        return $this;
+    }
+
+    public function getPriceWithDiscount()
+    {
+        // TODO: Implement getPriceWithDiscount() method.
+        return $this->getPrice() - ($this->getPrice() / 100) * $this->getDiscount();
     }
 
 }
@@ -60,22 +117,46 @@ class Notebooks extends Product
     {
         parent::__construct($price, $title);
 
-        return $this->pages = $pages;
-        return $this->form = $form;
+        $this->pages = $pages;
+        $this->form = $form;
     }
 
     public function getPages()
     {
         return $this->pages;
-        return $this;
     }
 
     public function getForm()
     {
         return $this->form;
-        return $this;
     }
 
 }
-$notebook1 = new Notebooks(150,'тетрадь школьная',48,'в клетку');
-$notebook1->getTitle()->getPrice()->getPages()->getForm();
+
+$notebook1 = new Notebooks(100, 'школьная тетрадь', 48, 'тетрадь в клетку');
+echo 'Товар : ' . $notebook1->getTitle() . '<br>';
+echo 'Цена : ' . $notebook1->getPrice() . '<br>';
+echo 'Страниц : ' . $notebook1->getPages() . '<br>';
+echo 'Формат : ' . $notebook1->getForm() . '<br>';
+$notebook1->setDelivery(250);
+echo 'Доставка : ' . $notebook1->getDelivery() . '<br>';
+
+echo '<hr>';
+
+$banana1 = new Bananas(1000, 'бананы', 15);
+echo 'Товар : ' . $banana1->getTitle() . '<br>';
+echo 'Цена : ' . $banana1->getPrice() . '<br>';
+echo 'Вес : ' . $banana1->getWeight() . 'кг<br>';
+echo 'скидка : ' . $banana1->getDiscountWeight() . '<br>';
+echo 'Доставка : ' . $banana1->getDelivery() . '<br>';
+
+echo '<hr>';
+
+$wine1 = new Wine(450, 'винишко донпериньен');
+echo 'Товар : ' . $wine1->getTitle() . '<br>';
+echo 'Цена : ' . $wine1->getPrice() . '<br>';
+echo 'Скидка : ' . $wine1->getDiscount() . '%<br>';
+echo 'Финальная цена : ' . $wine1->getPriceWithDiscount() . '<br>';
+$wine1->setDelivery(300);
+echo 'Доставка : ' . $wine1->getDelivery() . '<br>';
+
